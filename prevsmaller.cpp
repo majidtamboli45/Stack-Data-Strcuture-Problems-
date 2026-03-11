7bfcba27a01b0bdbeac5cb5a942eaca1d9ae1361
Majid Tamboli <169020141+majidtamboli45@users.noreply.github.com>
2026-03-11 17:50:19 +0530

    Add files via upload

---
+++ prevsmaller.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+vector<int> prevSmallerElement(vector<int> arr) {
+    vector<int> ans(arr.size());   // allocate space
+    stack<int> s;
+    
+    for(int i = 0; i < arr.size(); i++)
+    {
+        while(!s.empty() && s.top() >= arr[i])
+        {
+            s.pop();
+        }
+        
+        if(s.empty())
+        {
+            ans[i] = -1;
+        }
+        else
+        {
+            ans[i] = s.top();
+        }
+
+        s.push(arr[i]);
+    }
+
+    return ans;
+}
+
+int main() {
+
+    vector<int> arr = {3, 1, 0, 8, 6};
+
+    vector<int> ans = prevSmallerElement(arr);
+
+    for(int val : ans) {
+        cout << val << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
